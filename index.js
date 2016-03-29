c5ed57e3aae9be8b45e36934180e59a5eb2c84d1
Simon Elvery <simon@elvery.net>
2016-03-29 18:06:53 +1000

    Begin re-arranging to make this more than a list manager.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #! /usr/bin/env node
 
-var colors = require('colors');
-
 var argv = require('yargs')
-	.usage('\nUsage: tlm <command>')
-	.command('auth', 'Authorise with Twitter', require('./auth'))
-	.command('add <list> [accounts]', 'Add users to a list', require('./add'))
-	.command('remove <list> [accounts]', 'Remove users from a list', require('./remove'))
-	.demand(1, 'Must provide a valid command'.red)
+	.usage('\nUsage: tw <command>')
+	.command('auth', 'Authorise with Twitter', require('./lib/auth'))
+	.command('list-add <list> [accounts]', 'Add users to a list', require('./lib/list-add'))
+	.command('list-remove <list> [accounts]', 'Remove users from a list', require('./lib/list-remove'))
+	.demand(1, '')
 	.help('h')
 	.alias('h', 'help')
 	.argv;
